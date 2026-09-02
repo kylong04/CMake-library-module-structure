3d4ccbc5bfa477bbe8fef44e1a35cadffa282a06
kylong04 <kylongta2k4@gmail.com>
2026-09-02 15:53:03 +0700

    docs: add Readme.md

---
+++ Project1/app/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Greeter.h"
+
+int main() {
+  std::cout << get_greeting();
+  return 0;
+}
---
+++ Project1/greeter_test/src/Greeter.cpp
@@ -0,0 +1,5 @@
+#include "Greeter.h"
+
+std::string get_greeting() {
+  return "Hello from the Greeter library!";
+}
